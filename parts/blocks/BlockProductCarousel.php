75ca0c81091f2da1c59601a4ddf6afb133dfacab
EdwardN1 <edward@technicks.com>
2019-09-15 22:11:27 +0100

    Product Carousel

--- parts/blocks/BlockProductCarousel.php
+++ parts/blocks/BlockProductCarousel.php
@@ -2,30 +2,23 @@
 <?php global $post; ?>
 <div class="product-carousel">
     <div class="grid-container">
-        <h2 class="text-center"><?php echo $heading; ?></h2>
-		<?php if ( have_rows( 'slides' ) ) : ?>
-            <div data-slick-slider
-                 data-slick='{"slidesToShow":5, "slidesToScroll":5, "infinite":false, "initialSlide":1, "responsive": [{"breakpoint": 915,"settings": {"slidesToShow": 2.5, "slidesToScroll": 2}}, {"breakpoint": 550, "settings": {"slidesToShow": 1.1, "slidesToScroll": 1}}]}'>
-				<?php while ( have_rows( 'slides' ) ) :
-				the_row(); ?>
-                <div>
-					<?php $post_object = get_sub_field( 'products' ); ?>
-					<?php if ( $post_object ): ?>
-						<?php $post = $post_object; ?>
-						<?php setup_postdata( $post ); ?>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        <br> PostID = <?php echo $post->ID ?>
-                        <br> Code =<?php the_field( 'code' ); ?>
-                        <br> Price = <?php the_field( 'price' ); ?>
-						<?php wp_reset_postdata(); ?>
-					<?php endif; ?>
-					<?php endwhile; ?>
-                </div>
-            </div>
-		<?php else : ?>
-			<?php // no rows found ?>
-		<?php endif; ?>
+        <h2 class="text-center"><?php echo $heading; ?> - for PostID <?php echo $post->ID; ?></h2>
+        <div data-slick-slider
+             data-slick='{"slidesToShow":5, "slidesToScroll":5, "infinite":false, "initialSlide":1, "responsive": [{"breakpoint": 915,"settings": {"slidesToShow": 2.5, "slidesToScroll": 2}}, {"breakpoint": 550, "settings": {"slidesToShow": 1.1, "slidesToScroll": 1}}]}'>
+			<?php $post_objects = get_field( 'products' ); ?>
+			<?php if ( $post_objects ): ?>
+				<?php foreach ( $post_objects as $post ): ?>
+                    <div>
+					<?php setup_postdata( $post ); ?>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <br> Code = <?php the_field('code',$post->ID);?>
+                        <br> Price = <?php the_field('price',$post->ID);?>
+                    </div>
+				<?php endforeach; ?>
+
+				<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+        </div>
     </div>
-</div>
 
 
