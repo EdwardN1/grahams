91c2ce0d62fade47f2cf0e5f7a412083ab14dba6
EdwardN1 <edward@technicks.com>
2019-09-21 22:58:24 +0100

    Product Carousel and api

--- functions/api/api-responses.php
+++ functions/api/api-responses.php
@@ -1,79 +1,76 @@
 <?php
 // Create a stream
 $opts = array(
-'http' => array(
-'method' => "GET",
-'header' => "Ocp-Apim-Subscription-Key: e57667e8bffa4384a6ce53f80521677a"
-)
+	'http' => array(
+		'method' => "GET",
+		'header' => "Ocp-Apim-Subscription-Key: e57667e8bffa4384a6ce53f80521677a"
+	)
 );
 
 $context = stream_context_create( $opts );
 
+function displayJSON( $apiCall ) {
+	$allAttributes = new RecursiveIteratorIterator( new RecursiveArrayIterator( json_decode( $apiCall, true ) ), RecursiveIteratorIterator::SELF_FIRST );
+	echo '<div style="padding-bottom: 10px; padding-top: 10px; border-bottom: 1px solid black">';
+	echo '<p>';
+	$indent=1;
+	foreach ( $allAttributes as $key => $val ) {
+		if ( is_array( $val ) ) {
+			echo "</p><p>$key:<br>";
+		} else {
+			echo "<span style='padding-left: 1em;'> $key => $val</span><br>";
+		}
+
+	}
+	echo '</p>';
+	echo '</div>';
+}
+
 ?>
 
-<h3>Attributes - Get all</h3>
+<!--<h3>Attributes - Get all</h3>
 <?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Attributes/', false, $context );
-?>
+/*$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Attributes/', false, $context );
+displayJSON( $apiCall );
+*/?>
 
 <h3>Categories - Get all</h3>
 <?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Categories/', false, $context );
-?>
+/*$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Categories/', false, $context );
+displayJSON( $apiCall );
+*/?>
 
 <h3>Category - Gas</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Categories/14', false, $context );
-?>
+--><?php
+/*$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Categories/80', false, $context );
+displayJSON( $apiCall );
+*/?>
 
 <h3>Products - Get all</h3>
 <?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/', false, $context );
+$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/', false, $context );
+$allProducts = json_decode($apiCall);
+$limit = $allProducts->Limit;
+$TotalResults = $allProducts->TotalResults;
+$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/?limit='.$TotalResults, false, $context );
+$allProducts = json_decode($apiCall);
 ?>
 
-<h3>Product - 1806018</h3>
 <?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/1806018', false, $context );
+displayJSON( $apiCall );
 ?>
 
-<h3>Product Image - 1806018 - 16149</h3>
+<h3>All Products Iterated</h3>
 <?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Pictures/16149', false, $context );
-?>
-
-<h3>Product Image - 1806018 - 19649</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Pictures/19649', false, $context );
-?>
-
-
-<h3>Product - 1806022</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/1806022', false, $context );
-?>
-
-<h3>Product Image - 1806022 - 13354</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Pictures/13354', false, $context );
-?>
-
-<h3>Product Image - 1806022 - 14422</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Pictures/14422', false, $context );
-?>
-
-<h3>Product - 1806293</h3>
-<?php
-// Open the file using the HTTP headers set above
-echo file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/1806293', false, $context );
+$allProductsResults=$allProducts->Results;
+foreach ($allProductsResults as $productResult) {
+	$apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Products/'.$productResult->Id, false, $context );
+	displayJSON( $apiCall );
+	echo '<h4>Variations for '.$productResult->Id.'</h4>';
+	$productVariations = $productResult->VariationIds;
+	foreach ($productVariations as $productVariation) {
+	    $apiCall = file_get_contents( 'https://epim.azure-api.net/Grahams/api/Variations/'.$productVariation, false, $context );
+	    displayJSON($apiCall);
+    }
+}
 ?>
--- parts/blocks/BlockProductCarousel.php
+++ parts/blocks/BlockProductCarousel.php
@@ -2,23 +2,50 @@
 <?php global $post; ?>
 <div class="product-carousel">
     <div class="grid-container">
-        <h2 class="text-center"><?php echo $heading; ?> - for PostID <?php echo $post->ID; ?></h2>
-        <div data-slick-slider
-             data-slick='{"slidesToShow":5, "slidesToScroll":5, "infinite":false, "initialSlide":1, "responsive": [{"breakpoint": 915,"settings": {"slidesToShow": 2.5, "slidesToScroll": 2}}, {"breakpoint": 550, "settings": {"slidesToShow": 1.1, "slidesToScroll": 1}}]}'>
+        <h2 class="text-center"><?php echo $heading; ?></h2>
+        <div class="slider-container">
 			<?php $post_objects = get_field( 'products' ); ?>
 			<?php if ( $post_objects ): ?>
-				<?php foreach ( $post_objects as $post ): ?>
-                    <div>
+			<?php foreach ( $post_objects as $post ): ?>
+                <div class="carousel-slide">
 					<?php setup_postdata( $post ); ?>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        <br> Code = <?php the_field('code',$post->ID);?>
-                        <br> Price = <?php the_field('price',$post->ID);?>
-                    </div>
-				<?php endforeach; ?>
-
+					<?php if ( have_rows( 'images', $post->ID ) ) : ?>
+						<?php $imageFirstURL = ''; ?>
+						<?php while ( have_rows( 'images', $post->ID ) ) : the_row(); ?>
+							<?php $image = get_sub_field( 'image' ); ?>
+							<?php $imageURL = $image['url']; ?>
+							<?php $imageALT = $image['alt']; ?>
+							<?php if ( $image ) { ?>
+								<?php if ( $imageFirstURL == '' ):$imageFirstURL = $imageURL;endif; ?>
+							<?php } ?>
+						<?php endwhile; ?>
+					<?php else : ?>
+						<?php // no rows found ?>
+					<?php endif; ?>
+                    <a href="<?php the_permalink(); ?>" class="carousel-link-container">
+                        <div class="slide-image">
+                            <img src="<?php echo $imageFirstURL; ?>">
+                        </div>
+                        <div class="slide-title">
+							<?php the_title(); ?>
+                        </div>
+                        <div class="slide-code">
+							<?php the_field( 'code', $post->ID ); ?>
+                        </div>
+                        <div class="only">
+                            Only
+                        </div>
+                        <div class="slide-price">
+							£<?php the_field( 'price', $post->ID ); ?>
+                        </div>
+                    </a>
+                </div>
 				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
+			<?php endforeach; ?>
         </div>
+
+		<?php endif; ?>
     </div>
+</div>
 
 
