0ff29eb9a828d9aa822c12d09c97bb954ac86eca
EdwardN1 <GH?841241ed>
2020-07-28 15:29:07 +0100

    Import Accounts CSV

---
+++ functions/includes/autoinc-smtp.php
@@ -0,0 +1,13 @@
+<?php
+add_action( 'phpmailer_init', 'send_smtp_email' );
+function send_smtp_email( $phpmailer ) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host       = SMTP_HOST;
+    $phpmailer->SMTPAuth   = SMTP_AUTH;
+    $phpmailer->Port       = SMTP_PORT;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->Username   = SMTP_USERNAME;
+    $phpmailer->Password   = SMTP_PASSWORD;
+    $phpmailer->From       = SMTP_FROM;
+    $phpmailer->FromName   = SMTP_FROMNAME;
+}
